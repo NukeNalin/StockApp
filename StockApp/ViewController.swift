9f78757c95324536d91bb4a9e55e9718e21443e8
Nalin <nalinporwal10@gmail.com>
2021-11-15 15:10:52 +0530

    Initial Commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Nalin Porwal on 15/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
